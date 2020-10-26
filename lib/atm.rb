6658e637a8efdb8c3aebd71497bd0eff988762f2
Kyungin Na <KyunginNa@1.local>
2020-10-26 16:40:16 +0100

    added a funtion to display the receipt using the instance double and created a test for it.

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,11 +1,20 @@
+require 'date'
 class Atm
     attr_accessor :funds
 
     def initialize
         @funds = 1000
     end
+
+    def withdraw(amount, account)
+        case
+        when amount > account.balance
+            return
+        else  
+            @funds -= amount
+            account.balance = account.balance - amount
+            { status: true, message: 'success', date: Date.today, amount: amount}
     
-    def withdraw(amount)
-        @funds -= amount
+        end
     end
 end
