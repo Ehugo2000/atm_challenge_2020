dbf62d447944eb73d524a4d416c626bf3fca50e0
Kyungin Na <KyunginNa@1.local>
2020-10-27 15:47:38 +0100

    added a method to check expir_date, but the test failed, so I committed it to share the codes with the team

--- lib/atm.rb
+++ lib/atm.rb
@@ -14,13 +14,18 @@ class Atm
             { status: false, message: 'insufficient funds in ATM', date: Date.today}
         when incorrect_pin?(pin_code, account.pin_code)
             {status: false, message: 'wrong pin', date: Date.today}
+        when card_expired?(account.exp_date)
+            {status: false, message: 'card expired', date: Date.today}
         else  
             perform_transaction(amount, account)
-           
         end
     end
 
 private
+
+def card_expired?(exp_date)
+    Date.strptime(exp_date, '%m/%y') < Date.today
+end
 
 def incorrect_pin?(pin_code, actual_pin)
     pin_code != actual_pin
@@ -35,8 +40,8 @@ def insufficient_funds_in_account?(amount, account)
 end
 
 def perform_transaction(amount, account)
-@funds -= amount
-account.balance = account.balance - amount
-{ status: true, message: 'success', date: Date.today, amount: amount}
+    @funds -= amount
+    account.balance = account.balance - amount
+    { status: true, message: 'success', date: Date.today, amount: amount}
 end
 end
