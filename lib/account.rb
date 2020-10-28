c997b8358ed73b9c57da775a812fc2a0ab0ef0dc
ehugo <mihaivos@gmail.com>
2020-10-28 12:04:21 +0100

    Creating test and instance method for see if account is activated or deactivated

--- lib/account.rb
+++ lib/account.rb
@@ -10,10 +10,17 @@ class Account
     def initialize
         @pin_code = rand(1000..9999)
         @exp_date = set_expire_date
+        @account_status = :active
     end
 
     def set_expire_date
         Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
     end
+
+    def deactivate
+        @account_status = :deactivated
+    end
+
+
 
 end
