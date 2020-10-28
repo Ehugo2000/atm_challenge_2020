4ff720c682e6eb0cda79a01f3b9c224ec3b4897b
Kyungin Na <KyunginNa@1.local>
2020-10-28 11:40:02 +0100

    created a method to set expiration date and creted a test for it.

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,19 @@
+require 'date'
+
 class Account
     attr_accessor :pin_code
-    
+    attr_accessor :balance
+    attr_accessor :account_status
+    attr_accessor :exp_date
+    STANDARD_VALIDITY_YRS = 5
+
     def initialize
         @pin_code = rand(1000..9999)
+        @exp_date = set_expire_date
+    end
+
+    def set_expire_date
+        Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
     end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,7 +1,14 @@
 require './lib/account.rb'
+require 'date'
+
 describe Account do
     it 'check the length of pin code' do
         number_length = Math.log10(subject.pin_code).to_i + 1
         expect(number_length).to eq 4
+    end
+
+    it 'is expected to have an expiry date on initialize' do
+        expected_date = Date.today.next_year(5).strftime('%m/%y')
+        expect(subject.exp_date).to eq expected_date
     end
 end
